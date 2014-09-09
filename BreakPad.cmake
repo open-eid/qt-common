320e44b781a886609e3c5ab36ef21d0e170bb23c
Raul Metsma <raul@metsma.ee>
2014-09-09 15:37:02 +0300

    Move breakpad to qt-common submodule
    Ticket: #23847

---
+++ BreakPad.cmake
@@ -0,0 +1,18 @@
+set( BREAKPAD "https://cr.eesti.ee/" CACHE STRING "URL for breakpad crash reporting, empty for disabled" )
+if( BREAKPAD )
+	set( TESTING NO CACHE BOOL "Enable -crash parameter" )
+	if( TESTING OR "$ENV{ENABLE_CRASH}" )
+		add_definitions( -DTESTING )
+	endif()
+	add_definitions( -DBREAKPAD="${BREAKPAD}" )
+	find_program( DUMP_SYMS_EXECUTABLE dump_syms DOC "Path to dump_syms executable." )
+endif()
+
+macro( dump_syms PROGNAME )
+	if( DUMP_SYMS_EXECUTABLE )
+		add_custom_command(TARGET ${PROGNAME} POST_BUILD
+			COMMAND ${DUMP_SYMS_EXECUTABLE} $<TARGET_FILE:${PROGNAME}> > ${PROGNAME}.sym
+			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		)
+	endif()
+endmacro()
