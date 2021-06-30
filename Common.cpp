35bd8f2b0b33a75679813b7395408bf645e25580
Raul Metsma <raul@metsma.ee>
2021-06-30 20:02:32 +0300

    Use QSysInfo for OS version (#113)
    Fixes IB-6993, #74, #78
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- Common.cpp
+++ Common.cpp
@@ -23,23 +23,15 @@
 #include <QtCore/QDebug>
 #include <QtCore/QDir>
 #include <QtCore/QFileInfo>
-#include <QtCore/QProcess>
-#include <QtCore/QUrl>
-#include <QtCore/QUrlQuery>
+#include <QtCore/QOperatingSystemVersion>
 #include <QtCore/QSettings>
 #include <QtGui/QIcon>
 #include <QtGui/QTextDocument>
 #include <QtNetwork/QNetworkProxyFactory>
 #include <QtWidgets/QLabel>
 
-#include <cstdlib>
-
-#if defined(Q_OS_WIN)
-#include <QtCore/QLibrary>
+#ifdef Q_OS_WIN
 #include <qt_windows.h>
-#elif defined(Q_OS_MAC)
-#include <QtCore/QXmlStreamReader>
-#include <sys/utsname.h>
 #endif
 
 #ifndef COMMON_STATIC
@@ -57,6 +49,7 @@ Common::Common( int &argc, char **argv, const QString &app, const QString &icon 
 
 	Q_INIT_RESOURCE(common_tr);
 #if defined(Q_OS_WIN)
+	AllowSetForegroundWindow( ASFW_ANY );
 	setLibraryPaths({ applicationDirPath() });
 #elif defined(Q_OS_MAC)
 	qputenv("OPENSSL_CONF", applicationDirPath().toUtf8() + "../Resources/openssl.cnf");
@@ -66,90 +59,37 @@ Common::Common( int &argc, char **argv, const QString &app, const QString &icon 
 		"QDialogButtonBox { dialogbuttonbox-buttons-have-icons: 0; }\n"));
 
 	QNetworkProxyFactory::setUseSystemConfiguration(true);
-
-#if defined(Q_OS_WIN)
-	AllowSetForegroundWindow( ASFW_ANY );
-#endif
 }
 #endif
 
 QString Common::applicationOs()
 {
-#if defined(Q_OS_LINUX)
-	QProcess p;
-	p.start("lsb_release", { "-s", "-d" });
-	p.waitForFinished();
-	return QString::fromLocal8Bit( p.readAll().trimmed() );
-#elif defined(Q_OS_MAC)
-	struct utsname unameData;
-	uname( &unameData );
-	QFile f(QStringLiteral("/System/Library/CoreServices/SystemVersion.plist"));
-	if( f.open( QFile::ReadOnly ) )
-	{
-		QXmlStreamReader xml( &f );
-		while( xml.readNext() != QXmlStreamReader::Invalid )
-		{
-			if( !xml.isStartElement() || xml.name() != "key" || xml.readElementText() != QStringLiteral("ProductVersion"))
-				continue;
-			xml.readNextStartElement();
-			return QStringLiteral("Mac OS %1 (%2/%3)")
-				.arg( xml.readElementText() ).arg( QSysInfo::WordSize ).arg( unameData.machine );
-		}
-	}
+#if defined(Q_OS_MAC)
+	const auto version = QOperatingSystemVersion::current();
+	return QStringLiteral("%1 %2.%3.%4 (%5/%6)")
+		.arg(version.name())
+		.arg(version.majorVersion())
+		.arg(version.minorVersion())
+		.arg(version.microVersion())
+		.arg(QSysInfo::buildCpuArchitecture())
+		.arg(QSysInfo::currentCpuArchitecture());
 #elif defined(Q_OS_WIN)
-	OSVERSIONINFOEX osvi = { sizeof( OSVERSIONINFOEX ) };
-	if( GetVersionEx( (OSVERSIONINFO *)&osvi ) )
-	{
-		bool workstation = osvi.wProductType == VER_NT_WORKSTATION;
-		SYSTEM_INFO si;
-		typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
-		if( PGNSI pGNSI = PGNSI( QLibrary( "kernel32" ).resolve( "GetNativeSystemInfo" ) ) )
-			pGNSI( &si );
-		else
-			GetSystemInfo( &si );
-		QString os;
-		switch( (osvi.dwMajorVersion << 8) + osvi.dwMinorVersion )
-		{
-		case 0x0500: os = workstation ? "2000 Professional" : "2000 Server"; break;
-		case 0x0501: os = osvi.wSuiteMask & VER_SUITE_PERSONAL ? "XP Home" : "XP Professional"; break;
-		case 0x0502:
-			if( GetSystemMetrics( SM_SERVERR2 ) )
-				os = "Server 2003 R2";
-			else if( workstation && si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64 )
-				os = "XP Professional";
-			else
-				os = "Server 2003";
-			break;
-		case 0x0600: os = workstation ? "Vista" : "Server 2008"; break;
-		case 0x0601: os = workstation ? "7" : "Server 2008 R2"; break;
-		case 0x0602: os = workstation ? "8" : "Server 2012"; break;
-		case 0x0603: os = workstation ? "8.1" : "Server 2012 R2"; break;
-		case 0x0A00: os = (workstation ? "10" : "Server 10") + QString(" %1").arg(osvi.dwBuildNumber); break;
-		default: break;
-		}
-		QString extversion( (const QChar*)osvi.szCSDVersion );
-		return QString( "Windows %1 %2(%3 bit)" ).arg( os )
-			.arg( extversion.isEmpty() ? "" : extversion + " " )
-			.arg( si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64 ? "64" : "32" );
-	}
-	else
-	{
-		switch( QSysInfo::WindowsVersion )
-		{
-		case QSysInfo::WV_2000: return "Windows 2000";
-		case QSysInfo::WV_XP: return "Windows XP";
-		case QSysInfo::WV_2003: return "Windows 2003";
-		case QSysInfo::WV_VISTA: return "Windows Vista";
-		case QSysInfo::WV_WINDOWS7: return "Windows 7";
-		case QSysInfo::WV_WINDOWS8: return "Windows 8";
-		case QSysInfo::WV_WINDOWS8_1: return "Windows 8.1";
-		case QSysInfo::WV_WINDOWS10: return "Windows 10";
-		default: break;
-		}
-	}
+	QString product = QSysInfo::productType();
+	product[0] = product[0].toUpper();
+	QString version = QSysInfo::productVersion();
+	version.replace(QStringLiteral("server"), QStringLiteral("Server "));
+	return QStringLiteral("%1 %2 %3 (%4/%5)")
+		.arg(product)
+		.arg(version)
+		.arg(QOperatingSystemVersion::current().microVersion())
+		.arg(QSysInfo::buildCpuArchitecture())
+		.arg(QSysInfo::currentCpuArchitecture());
+#else
+	return QStringLiteral("%1 (%2/%3)").arg(
+		QSysInfo::prettyProductName(),
+		QSysInfo::buildCpuArchitecture(),
+		QSysInfo::currentCpuArchitecture());
 #endif
-
-	return tr("Unknown OS");
 }
 
 void Common::msgHandler(QtMsgType type, const QMessageLogContext &ctx, const QString &msg)
@@ -190,10 +130,10 @@ QString Common::language()
 	QString deflang;
 	switch( QLocale().language() )
 	{
-	case QLocale::Russian: deflang = "ru"; break;
-	case QLocale::Estonian: deflang = "et"; break;
+	case QLocale::Russian: deflang = QStringLiteral("ru"); break;
+	case QLocale::Estonian: deflang = QStringLiteral("et"); break;
 	case QLocale::English:
-	default: deflang = "en"; break;
+	default: deflang = QStringLiteral("en"); break;
 	}
-	return QSettings().value( "Language", deflang ).toString();
+	return QSettings().value(QStringLiteral("Language"), deflang).toString();
 }
